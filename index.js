5dc03a5e8408f1955b197b4594db2cad9338359d
Alexandra Roth <alroth8@gmail.com>
2017-03-17 15:12:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 function breakOut(array, changeValue, stopValue) {
-	for (let i = 0; i < array.length, i++){
-		if (i === stopValue) {
+	for (let i = 0; i < array.length; i++){
+		if (array[i] === stopValue) {
 			break
 		}
 		array[i] = changeValue
 	}
+	return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] == skipValue) {
+			continue
+		}
+		array[i] = changeValue
+	}
+	return array
+}
+
+function findBy(array, findFn) {
+	for (let i = 0; i < array.length; i++) {
+		if (findFn(array[i])) {
+			return array[i]
+		}
+	}
+	return null
 }
